eca846c81463ad1b750a1624c987c1c155d31ded
Игорь Пономарев <igor_p@live.ru>
2020-09-07 22:12:19 +0300

    Изменилось сообщение

---
+++ WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string Text { get; set; }
+
+        public void OnGet()
+        {
+            if (Text == null)
+            {
+                Text = "Привет!";
+            }
+        }
+
+        public void OnPost()
+        {
+            
+        }
+    }
+}
