5936e2e3d4b1ff08fb86248c7f102a98a1a34ad4
Игорь Пономарев <igor_p@live.ru>
2020-09-12 18:42:39 +0300

    Добавление страницы Index со списком Users

---
+++ WebApp/Pages/UserList/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+
+namespace WebApp.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        public AppDbContext db;
+
+        public List<User> Users { get; set; }
+
+        public void OnGet()
+        {
+            db = new AppDbContext();
+            Users = db.Users.ToList();
+        }
+    }
+}
